22410b73633ceabdeb5050927f0afc040cfa7473
Craig West <iwswordpress@gmail.com>
2024-09-04 17:52:28 +0100

    add rich to yield

--- tests/02_py_coffee/02_fixtures/03_yield_fixture/conftest.py
+++ tests/02_py_coffee/02_fixtures/03_yield_fixture/conftest.py
@@ -1,12 +1,14 @@
 """ conftest.py is a file that pytest will automatically import fixtures from """
 
 import pytest
+from rich.console import Console
 
+console = Console()
 
 # A simple fixture that generates some inputs
 @pytest.fixture
 def initial_value():
     """Doc"""
-    print("Providing a value to our test!")
+    console.print("\n[green]Providing a value to our test![/]")
     yield 5
-    print("Finishing Up!")
+    console.print("\n[dark_orange]Finishing Up![/]")
--- tests/02_py_coffee/02_fixtures/03_yield_fixture/test_yield_fixtures.py
+++ tests/02_py_coffee/02_fixtures/03_yield_fixture/test_yield_fixtures.py
@@ -1,5 +1,9 @@
 """ A simple example of test fixtures """
 
+from rich.console import Console
+
+console = Console()
+
 
 # Simple function that squares a number
 def square(num: int) -> int:
@@ -10,6 +14,6 @@ def square(num: int) -> int:
 # One test that uses our fixture
 def test_0087_square(initial_value: int) -> None:
     """Doc"""
-    print("Running our test!")
+    console.print("[cyan]Running our test![/]")
     result = square(initial_value)
     assert result == initial_value**2
