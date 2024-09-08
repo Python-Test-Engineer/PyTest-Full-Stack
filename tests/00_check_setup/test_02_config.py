fd6697105ad577be9951cad4a437709f7d3d60a5
Craig West <iwswordpress@gmail.com>
2024-09-08 18:52:29 +0100

    swap docstrings for comments

--- tests/00_check_setup/test_02_config.py
+++ tests/00_check_setup/test_02_config.py
@@ -1,15 +1,12 @@
-"""Some assert examples"""
-
-import pytest
+"""Test config"""
+from utils.read_config import get_version
 
 from rich.console import Console
 
 console = Console()
-from utils.read_config import get_version
 
-
+# An example to show using configparser with config.ini file
 def test_0011_SET_get_version():
-    """An example to show using configparser with config.ini file"""
     version = get_version()
     console.print(f"[blue]Version: {version}[/]")
     assert version
