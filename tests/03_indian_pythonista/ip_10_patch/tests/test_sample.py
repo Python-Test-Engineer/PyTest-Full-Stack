7263e2640118c2b230675e9831431bb75a4737b2
Craig West <iwswordpress@gmail.com>
2024-09-08 12:49:16 +0100

    ip 10 OK

--- tests/03_indian_pythonista/ip_10_patch/tests/test_sample.py
+++ tests/03_indian_pythonista/ip_10_patch/tests/test_sample.py
@@ -1,19 +1,19 @@
 from unittest import mock
 from unittest.mock import call
 
-from tut10.myapp.sample import random_sum, silly
+from ip_10_patch.myapp.sample import random_sum, silly
 
 
-@mock.patch("tut10.myapp.sample.random.randint")
+@mock.patch("ip_10_patch.myapp.sample.random.randint")
 def test_0114_random_sum(mock_randint):
     mock_randint.side_effect = [3, 4]
     assert random_sum() == 7
     mock_randint.assert_has_calls(calls=[call(1, 10), call(1, 7)])
 
 
-@mock.patch("tut10.myapp.sample.random.randint")
-@mock.patch("tut10.myapp.sample.time.time")
-@mock.patch("tut10.myapp.sample.requests.get")
+@mock.patch("ip_10_patch.myapp.sample.random.randint")
+@mock.patch("ip_10_patch.myapp.sample.time.time")
+@mock.patch("ip_10_patch.myapp.sample.requests.get")
 def test_0115_silly(mock_requests_get, mock_time, mock_randint):
     test_params = {"timestamp": 123, "number": 5}
     mock_time.return_value = test_params["timestamp"]
