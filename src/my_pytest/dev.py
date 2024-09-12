7adfeae6b3647262b6e918015c03e1f321d46fcc
Craig West <iwswordpress@gmail.com>
2024-09-12 14:42:10 +0100

    start my_pytest

---
+++ src/my_pytest/dev.py
@@ -0,0 +1,23 @@
+import dev_module as module_name
+
+
+all_funcs = dir(module_name)
+# print(all_funcs)
+function_names = [func for func in all_funcs if func.startswith("test_")]
+# print(function_names)
+
+
+# # Dictionary to store function names as keys and functions as values
+function_dict = {}
+
+# # Populate the dictionary
+for func_name in function_names:
+    # print(f"Adding function: {func_name}")
+    function_dict[func_name] = module_name.__dict__[func_name]
+
+print(function_dict)
+# # Now you can access and call the functions using the dictionary
+for func_name, func in function_dict.items():
+    print(f"\nCalling function: {func_name}")
+    actual_result = func()
+    print(f"Actual result: {actual_result}")
---
+++ src/my_pytest/dev_module.py
@@ -0,0 +1,13 @@
+def test_func1():
+    print("Function 1 called")
+    return 1
+
+
+def test_func2():
+    print("Function 2 called")
+    return 2
+
+
+def test_func3():
+    print("Function 3 called")
+    return 3
