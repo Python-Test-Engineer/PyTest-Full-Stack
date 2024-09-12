bad71692077d0fe9393b9fe88169315c11e72037
Craig West <iwswordpress@gmail.com>
2024-09-12 16:00:26 +0100

    refactor my_pytest

--- src/my_pytest/mytests.py
+++ src/my_pytest/mytests.py
@@ -1,29 +1,34 @@
 from fns import add, mul
 
+from rich.console import Console
+
+console = Console()
+
+
+def color_result(actual_result, expected_result):
+    if actual_result != expected_result:
+        console.print("[red bold]FAILED ❌[/]")
+    else:
+        console.print("[green bold]PASSED ✅[/]")
+
 
 def test_add():
-    print("Test add called")
+
     actual_result = add(5, 2)
     expected_result = 7
     print(f"Actual result: {actual_result} - Expected result: {expected_result}")
-    if actual_result != expected_result:
-        print("FAILED")
-    else:
-        print("PASSED")
+    color_result(actual_result, expected_result)
     assert (
         actual_result == expected_result
     ), f"Actual result should be {expected_result}"
 
 
 def test_add_fail():
-    print("Test add_fail called")
+
     actual_result = add(1, 2)
     expected_result = 6
     print(f"Actual result: {actual_result} - Expected result: {expected_result}")
-    if actual_result != expected_result:
-        print("FAILED")
-    else:
-        print("PASSED")
+    color_result(actual_result, expected_result)
     try:
         assert (
             actual_result == expected_result
@@ -33,28 +38,22 @@ def test_add_fail():
 
 
 def test_mul():
-    print("Test mul called")
+
     actual_result = mul(2, 3)
     expected_result = 6
     print(f"Actual result: {actual_result} - Expected result: {expected_result}")
-    if actual_result != expected_result:
-        print("FAILED")
-    else:
-        print("PASSED")
+    color_result(actual_result, expected_result)
     assert (
         actual_result == expected_result
     ), f"Actual result should be {expected_result}"
 
 
 def test_mul_fail():
-    print("Test mul_fail called")
+
     actual_result = mul(2, 2)
     expected_result = 9
     print(f"Actual result: {actual_result} - Expected result: {expected_result}")
-    if actual_result != expected_result:
-        print("FAILED")
-    else:
-        print("PASSED")
+    color_result(actual_result, expected_result)
     try:
         assert actual_result == expected_result, "Actual result should be 4"
     except Exception as e:
