cbf371e9aec71864c18f5056540b76d4dec2a298
Craig West <iwswordpress@gmail.com>
2024-09-29 09:32:25 +0100

    add async refernces

--- tests/00_check_setup/test_02_config.py
+++ tests/00_check_setup/test_02_config.py
@@ -18,7 +18,7 @@ def test_0011_SET_get_version():
     assert version == "1.0.0"
 
 
-def test_0012_SET_get_vapi_utl():
+def test_0012_SET_get_pet_api_url():
     api_url = get_pet_api_url()
     console.print(f"\n[yellow]API_URL: {api_url}[/]")
     assert api_url == "https://petstore.swagger.io/v2/pet/"
--- tests/06_async/README.md
+++ tests/06_async/README.md
@@ -1,1 +1,5 @@
+### Two good examples of PyTest and Asyncio
+
 https://bbc.github.io/cloudfit-public-docs/asyncio/testing.html
+
+https://pytest-with-eric.com/pytest-advanced/pytest-asyncio/
