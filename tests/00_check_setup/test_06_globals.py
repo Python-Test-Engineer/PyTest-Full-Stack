f84edcc742e4bfcd7ea0f1d3783749225aa58aa2
Craig West <iwswordpress@gmail.com>
2024-10-01 08:39:06 +0100

    reorg globals

--- tests/00_check_setup/test_06_globals.py
+++ tests/00_check_setup/test_06_globals.py
@@ -1,7 +1,5 @@
 """A simple example of tests using pytest"""
 
-# By Nick from CoffeeBeforeArch
-
 import pytest
 
 
@@ -18,15 +16,17 @@ def cube(num: int) -> int:
 
 
 # Using pytest.config for global values
-def test_0211_GEN_square():
-
+def test_0065_SET_globals(global_value):
+    print(global_value)
     num = 5
     result = square(num)
     assert result == num**2
 
 
 # Define another test in the same file
-def test_0212_GEN_1_cube():
+def test_0066_SET_request_config(request):
+    if request.config.my_global_value:
+        print(f"\nrequest.config.my_global_value: {request.config.my_global_value}")
     num = 5
     result = cube(num)
     assert result == num**3
